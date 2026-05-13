9d21991ffdeac8e78d8304c131cc80f6dc8aa08d
zw_devs <zw.devs.io@gmail.com>
2026-05-13 21:05:26 +0500

    Middleware branch pushed

--- src/middleware/cors.middleware.js
+++
@@ -1,9 +0,0 @@
-import cors from 'cors';
-
-export const corsMiddleware = cors({
-  origin: process.env.ALLOWED_ORIGINS?.split(',') || ['http://localhost:3000'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-});
-//not yet to push in the github.....
